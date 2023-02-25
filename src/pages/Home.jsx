31bb805cc14de5065468b5a6ca8592dffdc95451
Tariq <118397532+txriq03@users.noreply.github.com>
2023-02-25 22:11:22 +0000

    Made styling changes to navigation menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { Typography, CssBaseline, Box, Button, IconButton, useMediaQuery, AppBar, Toolbar, OutlinedInput, Drawer, Paper } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Menu,  KeyboardDoubleArrowRight, Search, Whatshot, Update, CalendarMonth, Interests, Cottage } from '@mui/icons-material';
-import { FaInstagram, FaTwitter, FaDiscord } from "react-icons/fa";
-import { useState, onMouseOver, onMouseEnter, onMouseLeave } from 'react'
+import {  FaDiscord } from "react-icons/fa";
+import { useState, onMouseOver, onMouseEnter, onMouseLeave } from 'react';
 import { Helmet } from 'react-helmet';
 import logo from '../assets/aniime.png';
 import './css/Home.css';
 import { styled } from '@mui/system';
-import { CSSTransition } from 'react-transition-group'
+import { CSSTransition } from 'react-transition-group';
 
 const theme = createTheme({
   palette: {
@@ -32,6 +32,12 @@ const theme = createTheme({
     },
   }
 });
+
+const StyledButton = styled(IconButton)`
+  &:hover {
+    background: none;
+  }
+`
 
 
 function Home() {
@@ -58,37 +64,31 @@ function Home() {
       <ThemeProvider theme={theme}>
       <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} elevation={0}>
         <Box p={2} sx={{display: 'flex', flexDirection: 'column'}}>
-          <CSSTransition 
-            in={isEnter}
-            timeout={1000}
-            className="HomeIcon"
-          >
-            <Paper className='HomeIcon' sx={{width: 80, height: 80, mt: 10, cursor: 'pointer', boxShadow: 10, borderRadius: 10, '&:hover': { borderRadius: 3, bgcolor: '#bd284d' }, transition: (theme) => theme.transitions.create('all')}} >
-              <IconButton sx={{ml: 1.2, mt: 1.2}} >
-                <Cottage style={{ fontSize: 45 }} color='primary' sx={{ '&:hover': { color: 'white' }, transition: (theme) => theme.transitions.create('all')}}/>
-              </IconButton>
-            </Paper>
-          </CSSTransition>
-          <Paper sx={{width: 80, height: 80, mt: 3, borderRadius: 10, boxShadow: 10, '&:hover': { borderRadius: 3, bgcolor: 'orange' }, transition: (theme) => theme.transitions.create('all')}}>
-            <IconButton sx={{ml: 1.2, mt: 1.2}} >
-              <Whatshot style={{ fontSize: 45 }} sx={{color: 'orange', '&:hover': { color: 'white' }, transition: (theme) => theme.transitions.create('all')}}/>
-            </IconButton>
-          </Paper>
-          <Paper sx={{width: 80, height: 80, mt: 3, borderRadius: 10, boxShadow: 10, '&:hover': { borderRadius: 3, bgcolor: '#2ec4b6' }, transition: (theme) => theme.transitions.create('all')}}>
-            <IconButton sx={{ml: 1.2, mt: 1.2}}>
-              <Update style={{ fontSize: 45 }} sx={{color: '#2ec4b6', '&:hover': { color: 'white' }, transition: (theme) => theme.transitions.create('all')}}/>
-            </IconButton>
-          </Paper>
-          <Paper sx={{width: 80, height: 80, mt: 3, borderRadius: 10, boxShadow: 10, '&:hover': { borderRadius: 3, bgcolor: '#BEEE62' }, transition: (theme) => theme.transitions.create('all')}}>
-            <IconButton sx={{ml: 1.2, mt: 1.2}}>
-              <CalendarMonth style={{ fontSize: 45 }} sx={{color: '#BEEE62', '&:hover': { color: 'white' }, transition: (theme) => theme.transitions.create('all')}}/>
-            </IconButton>
-          </Paper>
-          <Paper sx={{width: 80, height: 80, mt: 3, borderRadius: 10, boxShadow: 10, '&:hover': { borderRadius: 3, bgcolor: '#9649cb' }, transition: (theme) => theme.transitions.create('all')}}>
-            <IconButton sx={{ml: 1.2, mt: 1.2}}>
-              <Interests style={{ fontSize: 45 }} sx={{color: '#9649cb', '&:hover': { color: 'white' }, transition: (theme) => theme.transitions.create('all')}}/>
-            </IconButton>
-          </Paper>
+          <Box className='cottage-paper' onMouseEnter={handleMouseEnter}  >
+            <StyledButton sx={{ml: 1.2, mt: 1.2}} >
+              <Cottage className="cottage" style={{ fontSize: 45 }}  />
+            </StyledButton>
+          </Box>
+          <Box className="whatshot-paper">
+            <StyledButton sx={{ml: 1.2, mt: 1.2}} >
+              <Whatshot className="whatshot" style={{ fontSize: 45 }} />
+            </StyledButton>
+          </Box>
+          <Box className="update-paper">
+            <StyledButton sx={{ml: 1.2, mt: 1.2}}>
+              <Update className="update" style={{ fontSize: 45 }} />
+            </StyledButton>
+          </Box>
+          <Box className="calendar-paper">
+            <StyledButton sx={{ml: 1.2, mt: 1.2}}>
+              <CalendarMonth className="calendar" style={{ fontSize: 45 }} />
+            </StyledButton>
+          </Box>
+          <Box className="interests-paper">
+            <StyledButton sx={{ml: 1.2, mt: 1.2}}>
+              <Interests className="interests" style={{ fontSize: 45 }} />
+            </StyledButton>
+          </Box>
         </Box>
       </Drawer>
         <AppBar position="static" sx={{backgroundColor: 'black'}}>
@@ -112,7 +112,7 @@ function Home() {
             <OutlinedInput sx={{ml: 55, width: '500px', bgcolor: '#141414'}} size='small' startAdornment={<IconButton sx={{ml: -0.5}}> <Search/> </IconButton>} placeholder='Search...' >Search...</OutlinedInput>
 
             <Typography sx={{flexGrow: 1}}/>
-            <IconButton sx={{mr: 1}}>
+            <IconButton sx={{mr: 1}} href='https://discord.com/invite/qTPfvMxzNH'>
               <FaDiscord size='1.2em' color='#5562EA' />
             </IconButton>
             <Button variant='contained' size='medium' endIcon={<KeyboardDoubleArrowRight/>} sx={{mr: 10}} >Sign in</Button>
