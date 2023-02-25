9282135a9a97f5e9f9142b2fb63c56b94cecb049
Tariq <118397532+txriq03@users.noreply.github.com>
2023-02-25 22:42:12 +0000

    Added tooltips to menu nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Typography, CssBaseline, Box, Button, IconButton, useMediaQuery, AppBar, Toolbar, OutlinedInput, Drawer, Paper } from '@mui/material';
+import { Typography, CssBaseline, Box, Button, IconButton, useMediaQuery, AppBar, Toolbar, OutlinedInput, Drawer, Tooltip, Zoom } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Menu,  KeyboardDoubleArrowRight, Search, Whatshot, Update, CalendarMonth, Interests, Cottage } from '@mui/icons-material';
 import {  FaDiscord } from "react-icons/fa";
@@ -7,7 +7,6 @@ import { Helmet } from 'react-helmet';
 import logo from '../assets/aniime.png';
 import './css/Home.css';
 import { styled } from '@mui/system';
-import { CSSTransition } from 'react-transition-group';
 
 const theme = createTheme({
   palette: {
@@ -64,31 +63,41 @@ function Home() {
       <ThemeProvider theme={theme}>
       <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} elevation={0}>
         <Box p={2} sx={{display: 'flex', flexDirection: 'column'}}>
-          <Box className='cottage-paper' onMouseEnter={handleMouseEnter}  >
-            <StyledButton sx={{ml: 1.2, mt: 1.2}} >
-              <Cottage className="cottage" style={{ fontSize: 45 }}  />
-            </StyledButton>
-          </Box>
-          <Box className="whatshot-paper">
-            <StyledButton sx={{ml: 1.2, mt: 1.2}} >
-              <Whatshot className="whatshot" style={{ fontSize: 45 }} />
-            </StyledButton>
-          </Box>
-          <Box className="update-paper">
-            <StyledButton sx={{ml: 1.2, mt: 1.2}}>
-              <Update className="update" style={{ fontSize: 45 }} />
-            </StyledButton>
-          </Box>
-          <Box className="calendar-paper">
-            <StyledButton sx={{ml: 1.2, mt: 1.2}}>
-              <CalendarMonth className="calendar" style={{ fontSize: 45 }} />
-            </StyledButton>
-          </Box>
-          <Box className="interests-paper">
-            <StyledButton sx={{ml: 1.2, mt: 1.2}}>
-              <Interests className="interests" style={{ fontSize: 45 }} />
-            </StyledButton>
-          </Box>
+          <Tooltip title="Home" TransitionComponent={Zoom} placement='right'>
+            <Box className='cottage-paper' onMouseEnter={handleMouseEnter}  >
+              <StyledButton sx={{ml: 1.2, mt: 1.2}} >
+                <Cottage className="cottage" style={{ fontSize: 45 }}  />
+              </StyledButton>
+            </Box>
+          </Tooltip>
+          <Tooltip placement='right' TransitionComponent={Zoom} title='Trending'>
+            <Box className="whatshot-paper">
+              <StyledButton sx={{ml: 1.2, mt: 1.2}} >
+                <Whatshot className="whatshot" style={{ fontSize: 45 }} />
+              </StyledButton>
+            </Box>
+          </Tooltip>
+          <Tooltip placement='right' TransitionComponent={Zoom} title='Recently Updated'>
+            <Box className="update-paper">
+              <StyledButton sx={{ml: 1.2, mt: 1.2}}>
+                <Update className="update" style={{ fontSize: 45 }} />
+              </StyledButton>
+            </Box>
+          </Tooltip>
+          <Tooltip placement='right' TransitionComponent={Zoom} title='Calendar'>
+            <Box className="calendar-paper">
+              <StyledButton sx={{ml: 1.2, mt: 1.2}}>
+                <CalendarMonth className="calendar" style={{ fontSize: 45 }} />
+              </StyledButton>
+            </Box>
+          </Tooltip>
+          <Tooltip placement='right' TransitionComponent={Zoom} title='Categories'>
+            <Box className="interests-paper">
+              <StyledButton sx={{ml: 1.2, mt: 1.2}}>
+                <Interests className="interests" style={{ fontSize: 45 }} />
+              </StyledButton>
+            </Box>
+          </Tooltip>
         </Box>
       </Drawer>
         <AppBar position="static" sx={{backgroundColor: 'black'}}>
